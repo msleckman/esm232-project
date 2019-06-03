0e41c4c0cc2fa08fa4f54ceef6b342668bd14365
msleckman <msleckman@ucsb.edu>
2019-06-02 23:58:16 -0700

    word edit

--- transport_model1.R
+++ transport_model1.R
@@ -8,7 +8,7 @@
 #' @param daily_vehicle_reduction_proportion Default = 0 . Can reduce the daily vehicle miles travelled by a certain percentage (decimal format)
 #' @param proportion_diesel
 #' @param days_driven_peryear
-#' @param daily_vehicle_reduction_percent
+#' @param dailyVMT_change
 #' @references http://www.laalmanac.com/transport/tr01.php
 #' @references https://www.bts.gov/content/estimated-national-average-vehicle-emissions-rates-vehicle-vehicle-type-using-gasoline-and
 #' 
@@ -30,7 +30,7 @@ transport_emissions_LA = function(population_df,
   
   if(dailyVMT_change != 0){
     
-    dailyVMT_percar = dailyVMT_percar + (dailyVMT_percar * (dailyVMT_reduction_policy))
+    dailyVMT_percar = dailyVMT_percar + (dailyVMT_percar * (dailyVMT_change))
 
   }
   
@@ -88,7 +88,7 @@ transport_emissions_LA = function(population_df,
   annual_transport_emissions_kg = annual_transport_emissions_g/1000
 
   ## create DF
-  transport_df = data.frame(population_df[,1], population_df[,2],annual_transport_emissions_kg)
+  transport_df = data.frame(time=population_df[,1], pop=population_df[,2],annual_transport_emissions_kg)
   
   ## return
   return(transport_df)
@@ -96,4 +96,3 @@ transport_emissions_LA = function(population_df,
   }
 
 
-transport_emissions_LA(test)
